7533d79466f31be1c2f163b866f906ccb3aff425
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2025-09-24 12:38:26 +0600

    working with express

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer();
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 
-server.listen(3000);  
+app.use((req, res, next) => { 
+  next();  // allow the request to continue to the next middleware
+});
+
+app.use('/add-product', (req, res, next) => { 
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); // send a response to the client
+});
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => { 
+  res.send('<h1>Hello from Express!</h1>'); // send a response to the client
+});
+
+app.listen(3000)
