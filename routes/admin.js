52c0428fb2a50de04144e65a9509be15b4af8d52
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2025-09-25 11:30:02 +0600

    using pug as a template engine

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,20 @@ const path = require('path');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+
+const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => { 
-   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product'});
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
+    products.push({ title: req.body.title });
     console.log(req.body);
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
