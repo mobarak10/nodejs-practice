91dd4923a420d77bd8b712ba9e2024936d85b3e1
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2025-09-25 10:00:49 +0600

    working with express done

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => { 
-  next();  // allow the request to continue to the next middleware
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => { 
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); // send a response to the client
-});
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => { 
-  res.send('<h1>Hello from Express!</h1>'); // send a response to the client
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000)
